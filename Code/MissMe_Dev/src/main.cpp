ffb0fa75bb7b82a62f042383bc339ab21f83a3cf
Archie <archieatkinson97@gmail.com>
2020-01-21 01:26:32 +0000

    Added pin change interrupts

--- Code/MissMe_Dev/src/main.cpp
+++ Code/MissMe_Dev/src/main.cpp
@@ -9,11 +9,15 @@ const int button_list[kButtonInputs] = {2,3,4,5};
 
 unsigned long button_time = 0;  
 unsigned long last_button_time = 0; 
+volatile bool button_press_flag = true;
 
-void buttonpress();
+void ButtonDebounce();
+void PciInit();
 
 void setup() {
+  PciInit();
   Serial.begin(9600);
+
   for (int i = 0; i < kButtonInputs; i++){
     pinMode(button_list[i],INPUT_PULLUP);
   }
@@ -24,26 +28,43 @@ void setup() {
       pinMode(pin_list[i][j],OUTPUT);
     }
   }
-  attachInterrupt(digitalPinToInterrupt(4),buttonpress,FALLING);
+}
+
+ISR (PCINT2_vect){
+  ButtonDebounce();
 }
 
 void loop() {
   for(int i = 0; i < kPinBanks; i++){
     for(int j = 0; j < kPinAmounts; j++){
       digitalWrite(pin_list[i][j],HIGH);
+      while(1){
+        if (button_press_flag == true) break;
+      }      
       delay(300);
       digitalWrite(pin_list[i][j],LOW);
     }
-  }  
+  }
 }
 
-void buttonpress(){
+void ButtonDebounce(){
   button_time = millis();
   //check to see if increment() was called in the last 250 milliseconds
   if (button_time - last_button_time > 250)
   {
-    digitalWrite(6, HIGH);
+    if (button_press_flag == true){
+      button_press_flag = false;
+    }
+    else{
+      button_press_flag = true;
+    }
     last_button_time = button_time;
   }  
+}
 
+void PciInit(){
+  cli();
+  PCICR |= (1UL << PCIE2);
+  PCMSK2 |= (1UL << PCINT18) | (1UL << PCINT19) | (1UL << PCINT20) | (1UL << PCINT21);
+  sei();
 }
